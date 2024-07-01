6f9a1f9234ddbd752dc73b96be609d600a78decc
ishidanatsue <160199601+ishidanatsue@users.noreply.github.com>
2024-07-01 07:21:38 +0000

    committed 07012024

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,13 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+SELECT CustomerID FROM Customers
+WHERE FirstName='Norby';
+
+SELECT * FROM Reservations
+WHERE CustomerID=64;
+
+DELETE FROM Reservations WHERE ReservationID=2000;
+
+SELECT * FROM Reservations
+WHERE CustomerID=64;
